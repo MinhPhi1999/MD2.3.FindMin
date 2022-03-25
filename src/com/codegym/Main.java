c32d68fb3b052d7c5b669b6e60e1d02d0aa6a1a5
unknown <qquangmminh19999@gmail.com>
2022-03-25 14:58:28 +0700

    bt5

--- src/com/codegym/Main.java
+++ src/com/codegym/Main.java
@@ -3,13 +3,12 @@ package com.codegym;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        int[] arr = {4,12,7,8,1,-6,-9};
-        System.out.println("số nhỏ nhất là "+ minValue(arr));
+	int[] arr= {1,2,4,5,7,-5};
+        System.out.println("min là: "+ findMin(arr));
     }
-    public static int minValue(int [] arr){
+    static int findMin(int[] arr){
         int min = arr[0];
-        for (int i = 1; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             if (arr[i]<min){
                 min = arr[i];
             }
